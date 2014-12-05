a07e255401b4a6c399fc588509336379d954c77c
Simon Westerlund <s@simonwesterlund.se>
2014-12-05 16:08:17 +0100

    Updates docs

--- Source/NSLayoutConstraint+helper.h
+++ Source/NSLayoutConstraint+helper.h
@@ -10,16 +10,17 @@
 
 @interface NSLayoutConstraint (helper)
 
-/// Returns an array of constraints to set up views size
+/// Returns an array of constraints to set up a view's size
 + (NSArray *)wes_constraintWithItem:(id)view1 size:(CGSize)size;
 
-/// Returns a constraint that uses NSLayoutRelationEqual and sets the second attribute the same as the first one and with zero constant
+/// Returns a constraint with two views that will use have the same relation with that specific attribute
 + (instancetype)wes_constraintWithItem:(id)view1 attribute:(NSLayoutAttribute)attr1 toItem:(id)view2;
 
-/// Returns a constraint that uses NSLayoutRelationEqual and sets the second attribute the same as the first one
+/// Returns a constraint with two views that will use have the same relation with that specific attribute, however you can now set the constant
 + (instancetype)wes_constraintWithItem:(id)view1 attribute:(NSLayoutAttribute)attr1 toItem:(id)view2 constant:(CGFloat)c;
 
-/// Returns a constraint that uses NSLayoutRelationEqual
+/// Returns a constraint with two views that will use the equal relation, however, you're free to set two different attributes and a constant
 + (instancetype)wes_constraintWithItem:(id)view1 attribute:(NSLayoutAttribute)attr1 toItem:(id)view2 attribute:(NSLayoutAttribute)attr2 constant:(CGFloat)c;
 
 @end
+    
